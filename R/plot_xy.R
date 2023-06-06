3b95a24340143603de98c3c9288d4a1a3cb0ac83
Alec L. Robitaille <robit.alec@gmail.com>
2023-06-06 19:33:59 -0300

    simplify plot xy

--- R/plot_xy.R
+++ R/plot_xy.R
@@ -1,12 +1,7 @@
-plot_xy <- function(DT, x_col, y_col, title = NULL) {
+plot_xy <- function(DT, x_col, y_col) {
     stopifnot(x_col %in% colnames(DT))
     stopifnot(y_col %in% colnames(DT))
 
-    stopifnot(is.numeric(DT[[y_col]]))
-
-    if (is.null(title)) {
-        plot(DT[[x_col]], DT[[y_col]], xlab = x_col, ylab = y_col)
-    } else {
-        plot(DT[[x_col]], DT[[y_col]], main = title, xlab = x_col, ylab = y_col)
-    }
+    ggplot(DT) +
+      geom_point(aes({{x_col}}, {{y_col}}))
 }
