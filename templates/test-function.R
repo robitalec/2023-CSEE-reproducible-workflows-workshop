6180b5df91d9c9e4ebf79829fe0b2c2545720340
Alec L. Robitaille <robit.alec@gmail.com>
2023-06-06 11:56:30 -0300

    fix use counts

--- templates/test-function.R
+++ templates/test-function.R
@@ -20,7 +20,7 @@ source('R/plot_histogram.R')
 
 # Data --------------------------------------------------------------------
 # Load example data
-ice_area <- fread('https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-pal.88.8&entityid=b4062890db09a72628786650dacfbf1f')
+counts <- fread('https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-pal.88.8&entityid=b4062890db09a72628786650dacfbf1f')
 
 
 
