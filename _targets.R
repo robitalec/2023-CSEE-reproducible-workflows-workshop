22db422d4ba02e1f33557d3adf6127d2665e86a3
Alec L. Robitaille <robit.alec@gmail.com>
2023-06-09 14:15:09 -0300

    fill targets

--- _targets.R
+++ _targets.R
@@ -1,2 +1,26 @@
 library(targets)
+tar_source('R')
 
+
+# Variables
+path_counts <- file.path('raw-data', 'adelie-adult-chick-counts.csv')
+
+x_col <- 'date_gmt'
+y_col <- 'adults'
+
+
+# Targets
+c(
+    tar_target(
+        prep_counts,
+        prepare_csv(path_counts)
+    ),
+    tar_target(
+        sums,
+        sum_counts(prep_counts)
+    ),
+    tar_target(
+        plotted,
+        plot_xy(prep_counts, x_col, y_col)
+    )
+)
