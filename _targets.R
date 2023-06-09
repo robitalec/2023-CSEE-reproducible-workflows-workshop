882361f222168ae74c8cd5ef837cce7656f8a4a0
Alec L. Robitaille <robit.alec@gmail.com>
2023-06-09 17:09:31 -0300

    extend example, match exercise to it

--- _targets.R
+++ _targets.R
@@ -4,6 +4,8 @@ tar_source('R')
 
 # Variables
 path_counts <- file.path('raw-data', 'adelie-adult-chick-counts.csv')
+path_ice <- file.path('raw-data', 'ice-area.txt')
+path_weather <- file.path('raw-data', 'weather-timeseries.csv')
 
 x_col <- 'date_gmt'
 y_col <- 'adults'
@@ -11,16 +13,59 @@ y_col <- 'adults'
 
 # Targets
 c(
+    # Files
+    tar_target(
+        file_counts,
+        path_counts,
+        format = 'file'
+    ),
+    tar_target(
+        file_weather,
+        path_weather,
+        format = 'file'
+    ),
+ 
+
+    # Prepare
     tar_target(
         prep_counts,
-        prepare_csv(path_counts)
+        prepare_csv(file_counts)
     ),
     tar_target(
-        sums,
-        sum_counts(prep_counts)
+        prep_weather,
+        prepare_csv(file_weather)
     ),
+
+    # Group
+    tar_group_by(
+        group_counts,
+        prep_counts,
+        island
+    ),
+
+    # Sum counts by group
     tar_target(
-        plotted,
-        plot_xy(prep_counts, x_col, y_col)
+        sum_groups,
+        sum_counts(group_counts, 'island'),
+        pattern = map(group_counts)
+    ),
+
+    # Plot by group
+    tar_target(
+        plot_groups,
+        plot_xy(group_counts, x_col, y_col),
+        pattern = map(group_counts),
+        iteration = 'list'
+    ),
+
+    # Save plots
+    tar_target(
+        save_plot,
+        ggsave(filename = file.path('figures', group_counts[1, ]$island),
+               plot = plot_groups,
+               device = 'png'),
+        pattern = map(plot_groups, group_counts),
+        format = 'file',
+        iteration = 'list'
     )
 )
