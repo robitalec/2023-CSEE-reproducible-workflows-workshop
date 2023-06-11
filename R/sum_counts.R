50b1fec4851ea85b52e5d2bffe0341a3ec53dcb9
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2023-06-11 03:46:25 +0000

    Built site for gh-pages

--- R/sum_counts.R
+++
@@ -1,9 +0,0 @@
-sum_counts <- function(DF, by_cols, filter_gt_year = NULL) {
-    setDT(DF)
-    
-    if (!is.null(filter_gt_year)) {
-        DF <- DF[year(date_gmt) > filter_gt_year]
-    }
-
-    DF[, .(sum_adults = sum(adults), sum_chicks = sum(chicks)), by = by_cols]
-}
